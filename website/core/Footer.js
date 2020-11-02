55582ecbeb804f6ba52264524be8e98e13f4d515
Winz.Mawin7 <Winz.Mawin7@gmail.com>
2020-11-02 15:12:39 +0700

    Change linking and Remove Blog

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,52 +21,36 @@ class Footer extends React.Component {
   render() {
     const currentYear = new Date().getFullYear();
     return (
-      <footer className="nav-footer" id="footer">
-        <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
+      <footer className='nav-footer' id='footer'>
+        <section className='sitemap'>
+          <a href={this.props.config.baseUrl} className='nav-home'>
             {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
+              <img src={this.props.config.baseUrl + this.props.config.footerIcon} alt={this.props.config.title} width='66' height='58' />
             )}
           </a>
           <div>
             <h5>คู่มือ</h5>
-            <a href={this.docUrl("doc1.html")}>โรงเรียน/สถานศึกษา</a>
-            <a href={this.docUrl("doc2.html")}>ครูอาจารย์/บุคลากร</a>
-            <a href={this.docUrl("doc3.html")}>ผู้ปกครอง/นักเรียน</a>
+            <a href={this.docUrl('manual.html')}>โรงเรียน/สถานศึกษา</a>
+            <a href={this.docUrl('manual.html#สำหรับครูอาจารย์')}>ครูอาจารย์/บุคลากร</a>
+            <a href={this.docUrl('manual.html#สำหรับผู้ปกครอง')}>ผู้ปกครอง/นักเรียน</a>
           </div>
           <div>
             <h5>คุณสมบัติ</h5>
-            <a href={this.docUrl("clockin.html")}>ระบบบันทึกเวลา</a>
-            <a href={this.docUrl("scaninclass.html")}>ระบบเช็คชื่อรายวิชา</a>
-            <a href={this.docUrl("foodcourt.html")}>ระบบศูนย์อาหาร</a>
+            <a href={this.docUrl('clockin.html')}>ระบบบันทึกเวลา</a>
+            <a href={this.docUrl('scaninclass.html')}>ระบบเช็คชื่อรายวิชา</a>
+            <a href={this.docUrl('foodcourt.html')}>ระบบศูนย์อาหาร</a>
           </div>
           <div>
             <h5>อื่นๆ</h5>
-            <a href={this.props.config.baseUrl + "blog"}>Blog</a>
-            <a href="https://www.nextschool.io/?ref=docs">Nextschool.io</a>
+            {/* <a href={this.props.config.baseUrl + 'blog'}>Blog</a> */}
+            <a href='https://www.nextschool.io/?ref=docs'>Nextschool.io</a>
           </div>
         </section>
 
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          className="fbOpenSource"
-        >
-          <img
-            src={this.props.config.baseUrl + "img/oss_logo.png"}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
+        <a href='https://code.facebook.com/projects/' target='_blank' className='fbOpenSource'>
+          <img src={this.props.config.baseUrl + 'img/oss_logo.png'} alt='Facebook Open Source' width='170' height='45' />
         </a>
-        <section className="copyright">
-          Copyright &copy; {currentYear} Nextgensoft Co., Ltd.
-        </section>
+        <section className='copyright'>Copyright &copy; {currentYear} Nextgensoft Co., Ltd.</section>
       </footer>
     );
   }
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -34,44 +34,44 @@ const users = [
 ];
 
 const siteConfig = {
-  title: "NextSchool" /* title for your website */,
-  tagline: "คู่มือระบบบริหารสถานศึกษาครบวงจร",
-  url: "http://docs.nextschool.io/" /* your website url */,
-  baseUrl: "/" /* base url for your project */,
-  projectName: "nextschool",
+  title: 'NextSchool' /* title for your website */,
+  tagline: 'คู่มือระบบบริหารสถานศึกษาครบวงจร',
+  url: 'http://docs.nextschool.io/' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
+  projectName: 'nextschool',
   headerLinks: [
-    { doc: "manual", label: "คู่มือ" },
-    { doc: "system-overview", label: "คุณสมบัติ" },
-    { doc: "faq", label: "คำถามที่ถามบ่อย" },
-    { blog: true, label: "บล็อก" },
-    { search: true }
+    { doc: 'manual', label: 'คู่มือ' },
+    { doc: 'system-overview', label: 'คุณสมบัติ' },
+    { doc: 'faq', label: 'คำถามที่ถามบ่อย' },
+    // { blog: true, label: "บล็อก" },
+    { search: true },
   ],
   users,
   /* path to images for header/footer */
-  headerIcon: "img/nextschool_invert.svg",
-  footerIcon: "img/nextschool.svg",
-  favicon: "img/favicon.png",
+  headerIcon: 'img/nextschool_invert.svg',
+  footerIcon: 'img/nextschool.svg',
+  favicon: 'img/favicon.png',
   /* colors for website */
   colors: {
-    primaryColor: "#db5424",
-    secondaryColor: "#fb642d"
+    primaryColor: '#db5424',
+    secondaryColor: '#fb642d',
   },
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
-  copyright: "Copyright © " + new Date().getFullYear() + " Nextgensoft",
-  organizationName: "nextensions", // or set an env variable ORGANIZATION_NAME
-  projectName: "docs", // or set an env variable PROJECT_NAME
+  copyright: 'Copyright © ' + new Date().getFullYear() + ' Nextgensoft',
+  organizationName: 'nextensions', // or set an env variable ORGANIZATION_NAME
+  projectName: 'docs', // or set an env variable PROJECT_NAME
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: "default"
+    theme: 'default',
   },
-  scripts: ["https://buttons.github.io/buttons.js"],
+  scripts: ['https://buttons.github.io/buttons.js'],
   // You may provide arbitrary config keys to be used as needed by your template.
-  repoUrl: "https://github.com/nextensions/NSDoc",
+  repoUrl: 'https://github.com/nextensions/NSDoc',
   algolia: {
-    apiKey: "4fdd1e6cb553f663788dc3f2a3efd519",
-    indexName: "nextschool"
+    apiKey: '4fdd1e6cb553f663788dc3f2a3efd519',
+    indexName: 'nextschool',
   },
-  onPageNav: 'separate'
+  onPageNav: 'separate',
 };
 
 module.exports = siteConfig;
